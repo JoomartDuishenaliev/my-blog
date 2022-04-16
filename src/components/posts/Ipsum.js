746348808c4a7bf2e12239d92d773f51030b3c90
z to zee h <zhoomart.d@gmail.com>
2022-04-16 20:30:25 +0600

    feat: added header

---
+++ src/components/posts/Ipsum.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Ipsum = () => {
+    return (
+        <li className="w3-padding-16">
+            <img src="https://www.w3schools.com/w3images/gondol.jpg" alt="Image" className="w3-left w3-margin-right" style={{width: '50px'}} />
+            <span className="w3-large">Ipsum</span><br />
+            <span>Praes tinci sed</span>
+        </li>
+    );
+};
+
+export default Ipsum;
---
+++ src/components/posts/Lorem.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Lorem = () => {
+    return (
+        <li className="w3-padding-16">
+            <img src="https://www.w3schools.com/w3images/workshop.jpg" alt="Image" className="w3-left w3-margin-right"
+                 style={{width: '50px'}}/>
+            <span className="w3-large">Lorem</span><br/>
+            <span>Sed mattis nunc</span>
+        </li>
+    );
+};
+
+export default Lorem;
