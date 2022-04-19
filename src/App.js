003a1de9319371eb70d2c075d05f319e285c464b
z to zee h <zhoomart.d@gmail.com>
2022-04-20 05:42:01 +0600

    feat: counter done

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import React from "react";
-import Square from "./components/Square/Square";
+import CounterButtons from "./components/CounterButtons/CounterButtons";
 
 const App = () => {
     return (
-        <div className="w3-content" style={{maxWidth:"1400px"}}>
-            <Square color="red" size="500px"/>
-        </div>
+        <>
+            <div className="w3-content" style={{maxWidth:"1400px", display:"flex", justifyContent:"center"}}>
+                <div style={{display:"flex", justifyContent:"center", backgroundColor:"lightblue",
+                height:"200px", width:"300px"}}>
+                    <CounterButtons/>
+                </div>
+            </div>
+        </>
     )
 }
 
---
+++ src/components/CounterButtons/CounterButtons.js
@@ -0,0 +1,24 @@
+import React, {useState} from 'react';
+
+const CounterButtons = () => {
+    const [count, setCount] = useState(0);
+    return (
+        <div style={{display: "flex", flexDirection: "column"}}>
+            <div style={{display:"flex", justifyContent:"center"}}>
+                <h1>{count}</h1>
+            </div>
+            <div style={{display: "flex"}}>
+                <button onClick={() => setCount(count + 1)}
+                        style={{width: "75px", height: "40px", backgroundColor: "lime", marginRight:"40px"}}>
+                    Increase
+                </button>
+                <button onClick={() => setCount(count - 1)}
+                        style={{width: "85px", height: "40px", backgroundColor: "yellow"}}>
+                    Decrease
+                </button>
+            </div>
+        </div>
+    )
+}
+
+export default CounterButtons;
