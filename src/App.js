bd5c420b6b28be37c2cb1cf7eb90c970bd0b0262
z to zee h <zhoomart.d@gmail.com>
2022-04-22 22:36:03 +0600

    feat: post page

--- src/App.js
+++ src/App.js
@@ -1,7 +1,21 @@
 import React from "react";
-import Counter from "./components/Counter/Counter";
+import Header from "./components/header/Header";
+import BlogEntries from "./components/BlogEntries/BlogEntries";
+import PostDescription from "./components/PostDescription/PostDescription";
 import "./App.css";
 
-const App = () => <Counter/>
+const App = () => {
+    return (
+        <>
+            <div className="w3-content" style={{maxWidth: "1400px"}}>
+                <Header/>
+                <div className="w3-row">
+                    <BlogEntries/>
+                    <PostDescription/>
+                </div>
+            </div>
+        </>
+    )
+}
 
 export default App;
---
+++ src/components/PostDescription/PostDescription.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const PostDescription = () => {
+    return (
+        <div className="w3-col l8 s12">
+            <div className="w3-card-4 w3-margin w3-white">
+
+            </div>
+        </div>
+    )
+}
+
+export default PostDescription;
