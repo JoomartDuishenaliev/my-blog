c6c28296905cbe4e2febac386752ee770e0ec0ed
z to zee h <zhoomart.d@gmail.com>
2022-04-23 21:33:20 +0600

    feat: routing

--- src/App.js
+++ src/App.js
@@ -1,17 +1,20 @@
 import React from "react";
-import {BrowserRouter, Routes, Route} from "react-router-dom";
+import {BrowserRouter, Routes, Route, Link} from "react-router-dom";
 import Post from "./components/Pages/Post/Post";
 import "./App.css";
 import Main from "./components/Pages/Main/Main";
 
 const App = () => {
     return (
-        <BrowserRouter>
-            <Routes>
-                <Route path="index" element={<Main/>}/>
-                <Route path="post" element={<Post/>}/>
-            </Routes>
-        </BrowserRouter>
+        <>
+            <Main/>
+            <BrowserRouter>
+                <Link to="post"/>
+                <Routes>
+                    <Route path="post" element={<Post/>}/>
+                </Routes>
+            </BrowserRouter>
+        </>
     )
 }
 
--- src/components/BlogEntry/BlogEntry.js
+++ src/components/BlogEntry/BlogEntry.js
@@ -1,4 +1,6 @@
 import React from "react";
+import {BrowserRouter, Link, Route, Routes} from "react-router-dom";
+import Post from "../Pages/Post/Post";
 
 const BlogEntry = (props) => {
     console.log(props);
