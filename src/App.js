c438d8082907957739e5d2997ff8a47fa969baea
z to zee h <zhoomart.d@gmail.com>
2022-04-23 16:48:42 +0600

    feat: post page props added

--- src/App.js
+++ src/App.js
@@ -6,7 +6,18 @@ const App = () => {
     return (
         <>
             <div className="w3-content" style={{maxWidth: "1400px"}}>
-                <PostDescription/>
+                <PostDescription
+                    title="Main Title"
+                    desc="Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aspernatur deleniti magni modi mollitia
+                          nihil, quae quasi quia. Assumenda consequuntur earum enim est et excepturi nesciunt, officia
+                          perferendis praesentium quisquam ratione? Lorem ipsum dolor sit amet, consectetur
+                          adipisicing elit. Amet at beatae debitis dignissimos eligendi ex impedit magni porro quae, quia ratione
+                          repellat vero voluptatem. A alias at doloribus eos repellendus."
+                    img="https://images.unsplash.com/photo-1650450430541-9a95627ef2eb?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1206&q=80"
+                    prevComments="Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad asperiores commodi iure
+                                  omnis provident quasi, quibusdam repellendus voluptatibus! Debitis ex iure, quos
+                                  repudiandae sequi velit voluptatibus! Eos ex neque rem!"
+                />
             </div>
         </>
     )
--- src/components/PostDescription/PostDescription.js
+++ src/components/PostDescription/PostDescription.js
@@ -1,25 +1,16 @@
 import React from "react";
 
-const PostDescription = () => {
+const PostDescription = (props) => {
     return (
         <div className="w3-col l12 s12 card">
             <div className="w3-card-4 w3-margin w3-white">
-                <h1 className="title">Main Title</h1>
+                <h1 className="title">{props.title}</h1>
                 <div className="mainFlex">
                     <div className="leftFlex">
-                        <p className="largeFont">Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-                            Aspernatur
-                            deleniti magni modi mollitia
-                            nihil, quae quasi quia. Assumenda consequuntur earum enim est et excepturi nesciunt, officia
-                            perferendis praesentium quisquam ratione? Lorem ipsum dolor sit amet, consectetur
-                            adipisicing
-                            elit. Amet at beatae debitis dignissimos eligendi ex impedit magni porro quae, quia ratione
-                            repellat vero voluptatem. A alias at doloribus eos repellendus.</p>
+                        <p className="largeFont">{props.desc}</p>
                     </div>
                     <div className="rightFlex">
-                        <img
-                            src="https://images.unsplash.com/photo-1650450430541-9a95627ef2eb?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1206&q=80"
-                            alt="fjell"/>
+                        <img className="img" src={props.img} alt="mountain"/>
                     </div>
                 </div>
                 <hr className="line"/>
@@ -39,9 +30,7 @@ const PostDescription = () => {
                                 <span className="italicFont">10.10.2010</span>
                             </div>
                             <div className="commentSectionContent">
-                                <span className="commentSectionBorder">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad asperiores commodi iure
-                                    omnis provident quasi, quibusdam repellendus voluptatibus! Debitis ex iure, quos
-                                    repudiandae sequi velit voluptatibus! Eos ex neque rem!</span>
+                                <span className="commentSectionBorder">{props.prevComments}</span>
                             </div>
                         </div>
                         <div className="rowFlex">
@@ -50,7 +39,7 @@ const PostDescription = () => {
                                 <span className="italicFont">12.10.2013</span>
                             </div>
                             <div className="commentSectionContent">
-                                <span className="commentSectionBorder">Lorem ipsum dolor sit amet, consectetur adipisicing elit. A aliquid autem, blanditiis est eveniet id in maxime, nisi odit quibusdam repudiandae ullam, ut voluptatibus! Blanditiis culpa enim illum magnam reprehenderit!</span>
+                                <span className="commentSectionBorder">{props.prevComments}</span>
                             </div>
                         </div>
                     </div>
