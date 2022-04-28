86f35a91ff63f0020bf587c2a6246e747a552720
z to zee h <zhoomart.d@gmail.com>
2022-04-28 20:28:41 +0600

    feat: BrowserRouter into index.js

--- src/App.js
+++ src/App.js
@@ -2,16 +2,14 @@ import React from "react";
 import Main from "./components/Pages/Main/Main";
 import Post from "./components/Pages/Post/Post";
 import "./App.css";
-import {BrowserRouter, Routes, Route} from "react-router-dom";
+import {Routes, Route} from "react-router-dom";
 
 const App = () => {
     return (
-        <BrowserRouter>
-            <Routes>
-                <Route path="/" element={<Main/>}/>
-                <Route path="/post" element={<Post/>}/>
-            </Routes>
-        </BrowserRouter>
+        <Routes>
+            <Route path="/" element={<Main/>}/>
+            <Route path="/post" element={<Post/>}/>
+        </Routes>
     )
 }
 
--- src/components/BlogEntry/BlogEntry.js
+++ src/components/BlogEntry/BlogEntry.js
@@ -2,7 +2,6 @@ import React from "react";
 import {Link} from "react-router-dom";
 
 const BlogEntry = (props) => {
-    console.log(props);
     return (
         <>
             <div className="w3-card-4 w3-margin w3-white">
